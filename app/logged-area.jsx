eb6c74a5b347ced778a263601a3f0079615305af
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2023-11-16 21:12:38 -0300

    scroll

--- app/logged-area.jsx
+++ app/logged-area.jsx
@@ -1,5 +1,5 @@
 import { MaterialIcons } from '@expo/vector-icons';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, FlatList } from 'react-native';
 import { styles } from '../styles/styles.js';
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
@@ -8,24 +8,40 @@ import { fetchUserFunctions } from '../services/api';
 
 export default function LoggedArea({ navigation }) {
   const { user } = useContext(AuthContext);
-
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
   const [userFunctions, setUserFunctions] = useState([]);
 
   useEffect(() => {
     const loadUserFunctions = async () => {
       try {
-        const userFunctions = await fetchUserFunctions(user.profileType);
-        setUserFunctions(userFunctions);
+        setLoading(true);
+        const userFunctionsData = await fetchUserFunctions(user.profileType, page);
+        setUserFunctions((prevFunctions) => [...prevFunctions, ...userFunctionsData]);
+        setPage((prevPage) => prevPage + 1);
       } catch (error) {
+        console.error('Error loading user functions:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
     loadUserFunctions();
-  }, [user.profileType]);
+  }, [user.profileType, page]);
+
 
   const handleFunctionClick = (functionName) => {
     console.log(`Clicou na função: ${functionName}`);
   };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => handleFunctionClick(item.functionName)}>
+      <View style={styles.functionContainer}>
+        <MaterialIcons style={styles.functionIcon} size={100} name={item.icon} />
+        <Text style={styles.functionName}>{item.functionName}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.main}>
@@ -49,15 +65,17 @@ export default function LoggedArea({ navigation }) {
 
       </View>
 
+
       <View style={styles.loggedContainerRight}>
-        {userFunctions.map(({ functionName, icon }, index) => (
-          <TouchableOpacity key={index} onPress={() => handleFunctionClick(functionName)}>
-            <View style={styles.functionContainer}>
-              <MaterialIcons style={styles.functionIcon} size={100}name={icon} />
-              <Text style={styles.functionName} >{functionName}</Text>
-            </View>
-          </TouchableOpacity>
-        ))}
+        <FlatList
+          data={userFunctions}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => `${item.functionName}-${index}`}
+          numColumns={2}
+          columnWrapperStyle={styles.flatListColumnWrapper}
+          onEndReached={() => !loading && setPage((prevPage) => prevPage + 1)}
+          onEndReachedThreshold={0.1}
+        />
       </View>
     </View>
   );
